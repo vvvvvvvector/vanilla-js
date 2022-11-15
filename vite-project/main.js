ec4d65f14b7e8c2c592aa434f5bdc3ca662be3f8
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2022-11-16 00:29:56 +0100

    seminarium

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,23 +1,27 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
+const button = document.querySelector("button");
+const ul = document.querySelector("ul");
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+button.addEventListener("click", () => {
+  const input = document.createElement("input");
+  
+  button.innerText = "Dodaj";
+  
+  document.body.appendChild(input);
+  input.focus();
 
-setupCounter(document.querySelector("#counter"));
+  let text = "";
+  input.addEventListener("input", (event) => {
+    text = event.target.value;
+  });
+
+  input.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      const newLi = document.createElement("li");
+      newLi.innerText = text;
+      ul.appendChild(newLi);
+      document.body.removeChild(input);
+      button.innerText = "Dodaj nowe zainteresowanie";
+    }
+  });
+});
+
