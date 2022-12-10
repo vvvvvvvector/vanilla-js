991afc00b68ce997b0667749b45bb2b7d2373972
Viktar Zhdanovich <viktorzhdanovich@gmail.com>
2022-12-10 16:52:19 +0100

    Add checkbox for task. Refactoring.

--- todo/index.js
+++ todo/index.js
@@ -3,7 +3,7 @@ const addTaskButton = document.getElementsByTagName("button")[0];
 const ul = document.querySelector("ul");
 
 let taskText = "";
-const data = [];
+let data = [];
 
 document.addEventListener("DOMContentLoaded", () => {
   const items = localStorage.getItem("data");
@@ -12,9 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const tasks = JSON.parse(items);
 
     tasks.forEach((task) => {
-      data.push(task);
+      data = [...data, task];
       createListItem(task);
     });
+
+    console.log(data);
   }
 });
 
@@ -36,21 +38,33 @@ input.addEventListener("keyup", (event) => {
 addTaskButton.addEventListener("click", () => {
   if (taskText !== "") {
     if (data.length === 0) {
-      data.push({ id: 0, text: taskText });
-      localStorage.setItem("data", JSON.stringify(data));
+      data = [...data, { id: 0, text: taskText, checked: false }];
     } else {
-      data.push({ id: data[data.length - 1].id + 1, text: taskText });
-      localStorage.setItem("data", JSON.stringify(data));
+      data = [
+        ...data,
+        {
+          id: data[data.length - 1].id + 1,
+          text: taskText,
+          checked: false,
+        },
+      ];
     }
 
     createListItem(data[data.length - 1]);
     input.value = taskText = "";
+
+    localStorage.setItem("data", JSON.stringify(data));
   }
 });
 
 const createListItem = (task) => {
   const li = document.createElement("li");
   const div = document.createElement("div");
+
+  const checkbox = document.createElement("input");
+  checkbox.checked = task.checked;
+  checkbox.type = "checkbox";
+  checkbox.style.marginRight = "10px";
 
   const span = document.createElement("span");
   const button = document.createElement("button");
@@ -61,6 +75,17 @@ const createListItem = (task) => {
 
   button.innerText = "x";
   button.style = "display: none";
+
+  checkbox.addEventListener("change", () => {
+    const changedData = data.map((item) => {
+      if (item.id === task.id) {
+        item.checked = !item.checked;
+      }
+      return item;
+    });
+
+    localStorage.setItem("data", JSON.stringify(changedData));
+  });
 
   span.addEventListener("click", (event) => {
     event.target.style = "display: none";
@@ -102,22 +127,21 @@ const createListItem = (task) => {
 
     data.length = 0; // empty an array
 
-    filtered.forEach((i) => {
-      data.push(i);
+    const changedArr = filtered.map((item, index) => {
+      return {
+        ...item,
+        id: index,
+      };
     });
 
-    data.map((i, index) => {
-      i.id = index;
-    });
+    data = [].concat(changedArr);
 
     localStorage.setItem("data", JSON.stringify(data));
 
     event.target.closest("li").remove();
   });
 
-  div.appendChild(span);
-  div.appendChild(spanChangeInput);
-  div.appendChild(button);
+  div.append(checkbox, span, spanChangeInput, button);
 
   li.appendChild(div);
   ul.appendChild(li);
